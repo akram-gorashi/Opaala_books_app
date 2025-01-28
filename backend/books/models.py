c9dcbf83fe76705856b881ba000de3c76260c247
akram <akramgorashi6@gmail.com>
2025-01-28 20:24:35 +0300

    Added book and books list models and make migrations

--- backend/books/models.py
+++ backend/books/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    year = models.PositiveIntegerField()
+    author = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
+class BookList(models.Model):
+    name = models.CharField(max_length=255)
+    books = models.ManyToManyField(Book, related_name='lists')
+
+    def __str__(self):
+        return self.name
