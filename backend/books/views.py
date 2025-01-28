48da28d87f1a9b9c5ed3c52dd474a107f5203ecf
akram <akramgorashi6@gmail.com>
2025-01-28 20:52:35 +0300

    Added views for books list

--- backend/books/views.py
+++ backend/books/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
-
 # Create your views here.
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Book, BookList
+from .serializers import BookSerializer, BookListSerializer
+
+
+@api_view(['GET', 'POST'])
+def book_list_view(request):
+    if request.method == 'GET':
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'POST'])
+def booklist_view(request):
+    if request.method == 'GET':
+        booklists = BookList.objects.all()
+        serializer = BookListSerializer(booklists, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = BookListSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
